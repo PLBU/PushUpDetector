7b163cd44dddbea98803b7ecf0003c41c01b3709
Renaldi Arlin <johanes123renaldi@gmail.com>
2023-05-02 20:53:25 +0700

    Add Camera function

--- app/src/main/java/com/example/pushupdetector/MainActivity.java
+++ app/src/main/java/com/example/pushupdetector/MainActivity.java
@@ -1,14 +1,88 @@
 package com.example.pushupdetector;
 
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.camera.lifecycle.ProcessCameraProvider;
+import androidx.camera.core.Camera;
+import androidx.camera.core.CameraControl;
+import androidx.camera.core.CameraSelector;
+import androidx.camera.core.Preview;
+import androidx.camera.core.UseCaseGroup;
+import androidx.camera.core.ViewPort;
+import androidx.camera.view.PreviewView;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.google.common.util.concurrent.ListenableFuture;
+
+import java.util.concurrent.ExecutionException;
 
 public class MainActivity extends AppCompatActivity {
+    private PreviewView previewView;
+
+    private final ActivityResultLauncher<String> requestPermissionLauncher = registerForActivityResult(
+            new ActivityResultContracts.RequestPermission(),
+            granted -> {
+                if (!granted) {
+                    Toast.makeText(this, "Tolong berikan akses kamera", Toast.LENGTH_LONG).show();
+                }
+            }
+    );
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.CAMERA) == PackageManager.PERMISSION_DENIED) {
+            requestPermissionLauncher.launch(Manifest.permission.CAMERA);
+        }
+
+        previewView = findViewById(R.id.preview_view);
+        setCameraProviderListener();
+    }
+
+    private void setCameraProviderListener() {
+        ListenableFuture<ProcessCameraProvider> cameraProviderFuture =
+                ProcessCameraProvider.getInstance(this);
+        cameraProviderFuture.addListener(() -> {
+
+            try {
+                ProcessCameraProvider cameraProvider = cameraProviderFuture.get();
+                bindPreview(cameraProvider);
+            } catch (ExecutionException | InterruptedException e) {
+                // No errors need to be handled for this Future
+                // This should never be reached
+                e.printStackTrace();
+            }
+        }, ContextCompat.getMainExecutor(this));
+    }
+
+    private void bindPreview(ProcessCameraProvider cameraProvider) {
+        CameraSelector cameraSelector =
+                new CameraSelector.Builder().requireLensFacing(CameraSelector.LENS_FACING_BACK).build();
+
+        Preview preview = new Preview.Builder().build();
+
+        preview.setSurfaceProvider(previewView.getSurfaceProvider());
+
+        ViewPort viewPort = previewView.getViewPort();
+
+        if (viewPort != null) {
+            UseCaseGroup useCaseGroup = new UseCaseGroup.Builder()
+                    .addUseCase(preview)
+                    .setViewPort(viewPort)
+                    .build();
+
+            cameraProvider.unbindAll();
+            Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, useCaseGroup);
+            CameraControl cameraControl = camera.getCameraControl();
+            cameraControl.setLinearZoom((float) 0.3);
+        }
     }
 }
